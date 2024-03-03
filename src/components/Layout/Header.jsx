2f987057c0f5d1cf712f4f9ebb158fdfb658e0bd
mutyala srinivas <srinivasmutyala54@gmail.com>
2024-03-03 23:23:03 +0530

    header modified

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './HeaderStyles.css';
+ 
+
+const Header=()=>{
+ return(
+    <div className='header'>
+    <div className='header_left'>
+      <h1 className='header_title'>Venkateswara Foods</h1>
+    </div>
+    <div className='header_right'>
+    <a href='/'><img style={{width:"40px",height:'40px'}}
+    src='https://cdn.pixabay.com/photo/2015/12/23/01/14/edit-1105049_1280.png' 
+    alt='cart-pic'/> </a>
+    <a href='/'><img style={{width:"40px",height:'40px'}}
+    src='https://cdn-icons-png.flaticon.com/512/3237/3237472.png' 
+    alt='user-pic'/> </a> 
+    {/* <button className="header__button">Sign In</button> */}
+    </div>
+    </div>
+ )
+}
+
+export default Header;
