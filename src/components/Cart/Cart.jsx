c10aee7720d705c33ef4279739edee6cc586a9df
mutyala srinivas <srinivasmutyala54@gmail.com>
2024-06-02 23:56:01 +0530

    cart reduce functionality added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,24 @@
 import { useContext } from "react";
 import CartContext from "../../store/CartContext";
-
+ 
 const Cart=({onClose})=>{
    const cartCtx=useContext(CartContext);
    const {items,totalAmount}=cartCtx;
+   const removeHandler=(id)=>{
+    cartCtx.removeItem(id)
+    }
+    const reduceHandler=(id)=>{
+     cartCtx.decreaseQuantity(id)
+    }
   return(
     <div>
          <ul>
           {items.map(item=>(
             <li key={item.id}>
-              {item.title} * {item.quantity} - Rs{item.price.toFixed(2)}
+              {item.title} * {item.quantity} - Rs{item.price.toFixed(2)}-
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWC7V4IwtWtU9_UFg6_MJ1lmAwJpU-MksCtA&s"
+               style={{width:"20px",height:"20px",alignItems:"baseline", marginRight:"0px"}} alt="minimize" 
+               onClick={()=>reduceHandler(item.id)}/>
             </li>
           ))}
          </ul>
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -12,12 +12,35 @@ const CartProvider=({children})=>{
        setTotalAmount((prevTotal)=>prevTotal+item.price*item.quantity);
 
 };
-   const removeItemHandler=()=>{}
+const removeItemHandler=(id)=>{
+         setCartItem((prevItems) => {
+          const updatedItems = prevItems.filter(item => item.id !== id);
+          const itemToRemove = prevItems.find(item => item.id === id);
+          console.log(itemToRemove)
+          setTotalAmount((prevTotal) => prevTotal - itemToRemove.price * itemToRemove.quantity);
+          return updatedItems;
+        });
+     
+}
+
+const decreaseQuantityHandler = (id) => {
+    setCartItem((prevItems) => {
+        return prevItems.map(item => {
+            if (item.id === id && item.quantity > 1) {
+                return { ...item, quantity: item.quantity - 1 };
+            }
+            setTotalAmount((prevTotal) => prevTotal - item.price);  
+            return item;
+        });
+    });
+ };
+
     const cartContext={
         items:cartItem,
         totalAmount:totalAmount,
         addItem:addItemHandler,
-        removeItem:removeItemHandler
+        removeItem:removeItemHandler,
+        decreaseQuantity: decreaseQuantityHandler,
     }
     return(
         <CartContext.Provider value={cartContext}>
